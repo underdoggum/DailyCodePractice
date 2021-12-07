99bd544ff2a8d2e804ac5831b82f17f2bebf777a
Nathan Noack <nnoack3@gmail.com>
2021-12-07 18:45:46 -0500

    one more

--- js/diagonalDifference.js
+++ js/diagonalDifference.js
@@ -4,17 +4,23 @@
 // Problem: Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
 const diagonalDifference = arr => {
+  let ltrSum = 0;
+  let rtlSum = 0;
 
-  // find left-to-right diagonal sum
-
-
-
-  // find right-to-left diagonal sum
-
+  // find left-to-right and right-to-left diagonal sums
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[0].length; j++) {
+      if (i === j) {
+        ltrSum += arr[i][j];
+      }
+      if (j === arr.length - 1 - i) {
+        rtlSum += arr[i][j];
+      }
+    }
+  }
 
   // return absolute value of the difference between the diagonals
-
-
+  return Math.abs(ltrSum - rtlSum);
 }
 
 
@@ -24,6 +30,6 @@ const diagonalDifference = arr => {
 
 console.log(
 
-
+  diagonalDifference([[1,2,3,4],[1,4,5,6],[21,7,8,9]])
 
 )
