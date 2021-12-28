3eed0697d71a702be72e9b04759fea749490a7b9
Nathan Noack <nnoack3@gmail.com>
2021-12-28 10:44:33 -0500

    more

--- js/_scratch.js
+++ js/_scratch.js
@@ -12,7 +12,7 @@ const myFunc = arr => {
 
 
 console.log(
-
   
+  myFunc()
 
 )
---
+++ js/notVerySecure.js
@@ -0,0 +1,6 @@
+// Challenge Website: CodeWars
+// Title: Not Very Secure
+// URL: https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/javascript
+// Problem: In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+
+
